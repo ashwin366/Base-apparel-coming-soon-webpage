33b64670474c619fface3d8d0046234f5a44ee9c
Ashwin <ashwin.ashwin366@gmiail.com>
2020-12-24 19:38:50 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const button = document.querySelector(".btn");
 const emailInput = document.querySelector(".item1__email-input");
-const pattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
-button.addEventListener("click",function(){
-	if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailInput.value)){
-		emailInput.classList.remove("input--error");
-		document.querySelector(".error").classList.add("error--hidden");
-		this.classList.toggle('btn--active');
-		emailInput.classList.toggle('input--active');
-		setTimeout(function(){
-			document.querySelector(".item1__form").submit();
-			alert("Thank you for subscribing.");
-		},1000);
-	}else{
-		document.querySelector(".error").classList.remove("error--hidden");
-		emailInput.classList.add("input--error");
-	}
+button.addEventListener("click", function() {
+  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailInput.value)) {
+    emailInput.classList.remove("input--error");
+    document.querySelector(".error").classList.add("error--hidden");
+    this.classList.toggle('btn--active');
+    emailInput.classList.toggle('input--active');
+    setTimeout(function() {
+      document.querySelector(".item1__form").submit();
+      alert("Thank you for subscribing.");
+    }, 1000);
+  } else {
+    document.querySelector(".error").classList.remove("error--hidden");
+    emailInput.classList.add("input--error");
+  }
 
 });
